98de57e2b3cfbb8b3ee62be8589276292350d716
Jens Schumann <jens.schumann@openknowledge.de>
2007-02-02 23:03:13 +0000

    - Initial RRD4j code

---
+++ modules/jetm-contrib/src/java/etm/contrib/rrd/core/OfflineLogParser.java
@@ -0,0 +1,126 @@
+/*
+ *
+ * Copyright (c) 2004, 2005, 2006, 2007 void.fm
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name void.fm nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package etm.contrib.rrd.core;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Parses arbitrary files written by {@link etm.contrib.aggregation.log.AbstractLogAggregator}. Assumes
+ * {@link etm.contrib.aggregation.log.DefaultOutputFormatter} as default format.
+ *
+ * @author void.fm
+ * @version $Revision$
+ * @since 1.2.0
+ */
+public class OfflineLogParser {
+
+  private static final String DEFAULT_SCAN_PATTERN = "^(.*)measurementPoint=<([\\w\\s\\d]*)>, parent=<([\\w\\s]*)>, transactionTime=<(\\d*[,.]\\d*)>, recordingTime=<(\\d*)>";
+  private String pattern = DEFAULT_SCAN_PATTERN;
+
+  private List listener;
+  private NumberFormat numberFormat;
+
+
+  public OfflineLogParser() {
+    listener = new ArrayList();
+
+    // todo?? pattern
+    numberFormat = NumberFormat.getInstance();
+    numberFormat.setMinimumFractionDigits(3);
+    numberFormat.setMaximumFractionDigits(3);
+    numberFormat.setGroupingUsed(false);
+  }
+
+
+  public void setPattern(String aPattern) {
+    pattern = aPattern;
+  }
+
+  public void register(RrdExecutionListener aListener) {
+    listener.add(aListener);
+  }
+
+  public void parse(File aFile) throws IOException {
+    Pattern regex = Pattern.compile(pattern);
+
+    BufferedReader in = new BufferedReader(new FileReader(aFile));
+
+    try {
+      for (int i = 0; i < listener.size(); i++) {
+        RrdExecutionListener rrdListener = (RrdExecutionListener) listener.get(i);
+        rrdListener.onBegin();
+      }
+
+
+      String line;
+      while ((line = in.readLine()) != null) {
+        try {
+          Matcher matcher = regex.matcher(line);
+          if (matcher.matches()) {
+            OfflineExecution result = new OfflineExecution(
+              matcher.group(2),
+              matcher.group(3),
+              Long.parseLong(matcher.group(5)),
+              numberFormat.parse(matcher.group(4)).doubleValue()
+            );
+            for (int i = 0; i < listener.size(); i++) {
+              RrdExecutionListener rrdListener = (RrdExecutionListener) listener.get(i);
+              rrdListener.onNextMeasurement(result);
+            }
+
+          }
+        } catch (ParseException e) {
+          e.printStackTrace();
+        }
+      }
+      for (int i = 0; i < listener.size(); i++) {
+        RrdExecutionListener rrdListener = (RrdExecutionListener) listener.get(i);
+        rrdListener.onFinish();
+      }
+
+    } finally {
+      in.close();
+    }
+  }
+
+}
+
+
---
+++ modules/jetm-contrib/src/java/etm/contrib/rrd/rrd4j/Rrd4jAggregationWriter.java
@@ -0,0 +1,110 @@
+/*
+ *
+ * Copyright (c) 2004, 2005, 2006, 2007 void.fm
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name void.fm nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package etm.contrib.rrd.rrd4j;
+
+import etm.contrib.rrd.core.AbstractRrdExecutionListener;
+import etm.core.monitor.EtmPoint;
+import org.rrd4j.core.RrdDb;
+import org.rrd4j.core.Sample;
+import org.rrd4j.core.Util;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * A Rrd4j DB Writer that creates ...
+ *
+ * @author void.fm
+ * @version $Revision$
+ * @since 1.2.0
+ */
+public class Rrd4jAggregationWriter extends AbstractRrdExecutionListener {
+
+  private RrdDb db;
+
+
+  /**
+   * Creates a new writer that stores
+   *
+   * @param aDb A writeable RRD DB.
+   * @throws IOException Thrown by RRD operation
+   * @throws IllegalArgumentException If the rrd db definition does not contain all required
+   *                                  datasources
+   */
+  public Rrd4jAggregationWriter(RrdDb aDb) throws IOException {
+    super(aDb.getLastUpdateTime(), aDb.getRrdDef().getStep());
+
+    validateDataSource(aDb, "transactions");
+    validateDataSource(aDb, "min");
+    validateDataSource(aDb, "max");
+    validateDataSource(aDb, "average");
+
+    db = aDb;
+  }
+
+
+  public void onBegin() {
+
+  }
+
+
+  public void onFinish() {
+
+  }
+
+  protected long calculateTimestamp(EtmPoint measurement) {
+    return Util.getTimestamp(new Date(measurement.getStartTime()));
+  }
+
+  protected void flushStatus() {
+    if (transactions > 0) {
+      try {
+        Sample sample = db.createSample(endInterval);
+        sample.setValue("transactions", transactions);
+        sample.setValue("min", min);
+        sample.setValue("max", max);
+        sample.setValue("average", total / (double) transactions);
+
+        sample.update();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  protected void validateDataSource(RrdDb aDb, String name) throws IOException {
+    if (aDb.getDatasource(name) == null) {
+      throw new IllegalArgumentException("DataSource " + name + " not found");
+    }
+  }
+
+}
