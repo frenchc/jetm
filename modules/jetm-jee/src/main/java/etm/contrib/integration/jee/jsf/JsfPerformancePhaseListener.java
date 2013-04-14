e1516d515010d9a4258459ce79e1feabee2fac73
Jens Schumann <jens.schumann@openknowledge.de>
2013-04-14 20:55:07 +0000

    JSF integration. Further JSF demo content.

---
+++ modules/jetm-jee/src/main/java/etm/contrib/integration/jee/jsf/JsfPerformancePhaseListener.java
@@ -0,0 +1,106 @@
+/*
+ *
+ * Copyright (c) void.fm
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name void.fm nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package etm.contrib.integration.jee.jsf;
+
+import etm.core.configuration.EtmManager;
+import etm.core.monitor.EtmPoint;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author void.fm
+ * @version $Revision: 372 $
+ */
+public class JsfPerformancePhaseListener implements PhaseListener {
+
+  public static final String ROOT_ETM_POINT = "ETM__RequestPoint";
+  private static final String CURRENT_PHASE_POINT = "ETM__CurrentPhasePoint";
+
+
+  public void beforePhase(PhaseEvent event) {
+    FacesContext facesContext = event.getFacesContext();
+
+    if (event.getPhaseId().equals(PhaseId.RESTORE_VIEW)) {
+      EtmPoint requestPoint = EtmManager.getEtmMonitor().createPoint(getDefaultRequestName(facesContext));
+      facesContext.getAttributes().put(ROOT_ETM_POINT, requestPoint);
+    }
+
+    EtmPoint oldPoint = (EtmPoint) facesContext.getAttributes().get(CURRENT_PHASE_POINT);
+    if (oldPoint != null) {
+      oldPoint.alterName(oldPoint.getName() + " - uncollected");
+      oldPoint.collect();
+    }
+
+    EtmPoint point = EtmManager.getEtmMonitor().createPoint("JSF Phase " + event.getPhaseId().getOrdinal()
+      + " - " + String.valueOf(event.getPhaseId()));
+    facesContext.getAttributes().put(CURRENT_PHASE_POINT, point);
+  }
+
+  public void afterPhase(PhaseEvent event) {
+    FacesContext facesContext = event.getFacesContext();
+    EtmPoint point = (EtmPoint) facesContext.getAttributes().get(CURRENT_PHASE_POINT);
+    if (point != null) {
+      point.collect();
+      facesContext.getAttributes().put(CURRENT_PHASE_POINT, null);
+    }
+
+    if (event.getFacesContext().getResponseComplete() || event.getPhaseId().equals(PhaseId.RENDER_RESPONSE)) {
+      EtmPoint requestPoint = (EtmPoint) facesContext.getAttributes().get(ROOT_ETM_POINT);
+      if (requestPoint != null) {
+        requestPoint.collect();
+      }
+
+    }
+  }
+
+  public PhaseId getPhaseId() {
+    return PhaseId.ANY_PHASE;
+  }
+
+  protected String getDefaultRequestName(FacesContext context) {
+    HttpServletRequest httpServletRequest = (HttpServletRequest) context.getExternalContext().getRequest();
+    String request = httpServletRequest.getRequestURI();
+    int index = request.indexOf(';');
+    if (index > 0) {
+      request = request.substring(0, index);
+    }
+
+    String method = httpServletRequest.getMethod();
+    return "HTTP " + method + " " + request;
+  }
+
+}
---
+++ modules/jetm-jee/src/main/java/etm/contrib/integration/jee/jsf/NameInterceptingActionListener.java
@@ -0,0 +1,80 @@
+/*
+ *
+ * Copyright (c) void.fm
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name void.fm nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package etm.contrib.integration.jee.jsf;
+
+import etm.core.monitor.EtmPoint;
+
+import javax.el.MethodExpression;
+import javax.faces.component.ActionSource;
+import javax.faces.component.ActionSource2;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.el.MethodBinding;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+
+/**
+ * @author void.fm
+ * @version $Revision: 372 $
+ */
+public class NameInterceptingActionListener implements ActionListener {
+
+  private ActionListener delegate;
+
+  public NameInterceptingActionListener(ActionListener aDelegate) {
+    delegate = aDelegate;
+  }
+
+  public void processAction(ActionEvent actionEvent) throws AbortProcessingException {
+    UIComponent component = actionEvent.getComponent();
+    EtmPoint etmRootPoint =
+      (EtmPoint) FacesContext.getCurrentInstance().getAttributes().get(JsfPerformancePhaseListener.ROOT_ETM_POINT);
+    if (etmRootPoint != null) {
+      MethodExpression methodExpression = null;
+      if (component instanceof ActionSource2) {
+        methodExpression = ((ActionSource2) component).getActionExpression();
+        etmRootPoint.alterName("JSF Action " + methodExpression.getExpressionString());
+      }
+      if (methodExpression == null && component instanceof ActionSource) {
+        MethodBinding methodBinding = ((ActionSource) component).getAction();
+        etmRootPoint.alterName("JSF Action " + methodBinding.getExpressionString());
+
+      }
+
+
+    }
+
+    delegate.processAction(actionEvent);
+  }
+}
